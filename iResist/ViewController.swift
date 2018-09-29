520beabf1d92d7a537326287d111ecfc4f57453d
Jacob Farkas <jacobf@rkas.net>
2018-09-29 14:33:45 -0700

    Initial commit. Every app needs an icon to start

---
+++ iResist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iResist
+//
+//  Created by Jacob Farkas on 9/29/18.
+//  Copyright © 2018 Farktronix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
