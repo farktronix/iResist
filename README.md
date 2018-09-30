d991cce88c41b8551d217b0a705f283a4b021986
Jacob Farkas <jacobf@rkas.net>
2018-09-29 19:12:46 -0700

    Adding Travis CI support

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 ![App Icon](Resources/iResist_Icon.png)
 
+[![Build Status](https://travis-ci.com/farktronix/iResist.svg?branch=master)](https://travis-ci.com/farktronix/iResist.svg?branch=master)
+
 A simple resistance calculator for iOS
---
+++ xcodebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+command -v xcpretty >/dev/null 2>&1 || {
+    gem install xcpretty xcpretty-travis-formatter 2>&1 || { 
+        echo "Failed to install xcpretty. Aborting"
+        exit 1
+    }
+    command -v xcpretty >/dev/null 2>&1 || { 
+        echo "Still couldn't find xcpretty after installing it. Womp womp"
+        exit 1
+    }
+}
+
+# bundle exec xcpretty -v
+set -o pipefail && xcodebuild clean build -sdk iphonesimulator -project iResist.xcodeproj -scheme iResist CODE_SIGNING_REQUIRED=NO | bundle exec xcpretty -f `xcpretty-travis-formatter`
+
